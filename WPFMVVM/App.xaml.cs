8c40085c75ac382765fa57b64c628b805bd24834
LOlaye-Felix <LOlaye-Felix@UK-LEI-SOFTWARE>
2026-01-13 14:59:02 +0000

    Add project files.

---
+++ WPFMVVM/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+namespace WPFMVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        [DllImport("kernel32.dll")]
+        static extern bool AllocConsole();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AllocConsole();
+
+            InitialiseServices();
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+        }
+
+        private void InitialiseServices()
+        {
+            
+        }
+
+    }
+
+}
